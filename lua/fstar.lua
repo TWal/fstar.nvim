86e95b7904add9e706f0a16a8477e7978246d5a3
Théophile Wallez <theophile@wallez.net>
2023-10-02 22:29:55 +0200

    feat: add support for status update

--- lua/fstar.lua
+++ lua/fstar.lua
@@ -1,16 +1,33 @@
+local status = require("status")
 local M = {}
 
-local setup_lsp = function (fstar_lsp_path)
+local setup_lsp = function (fstar_lsp_path, namespace_id)
+    local status_displayer = status.StatusDisplayer:new(namespace_id, 0)
     vim.lsp.start({
         cmd = { fstar_lsp_path },
         root_dir = vim.fn.getcwd(), -- Use PWD as project root dir.
+        handlers = {
+            ["fstar-lsp/clearStatus"] = vim.lsp.with(
+                function(err, result, ctx, config)
+                    status_displayer:clear()
+                    return { success = true }
+                end,
+            {}),
+
+            ["fstar-lsp/setStatus"] = vim.lsp.with(
+                function(err, result, ctx, config)
+                    status_displayer:set_status(result.range.start.line, result.range["end"].line, result.statusType)
+                    return { success = true }
+                end,
+            {}),
+        }
     })
 end
 
 local fstar_verify_all = function()
     for _, client in ipairs(vim.lsp.get_active_clients({bufnr = 0})) do
         client.notify("fstar-lsp/verifyAll", {
-            ["textDocument"] = vim.lsp.util.make_text_document_params()
+            textDocument = vim.lsp.util.make_text_document_params()
         })
     end
 end
@@ -33,10 +50,24 @@ local setup_fstar_command = function ()
 end
 
 M.setup = function(cfg)
+    local namespace_id = vim.api.nvim_create_namespace("fstar.nvim")
+    vim.api.nvim_set_hl_ns(namespace_id)
+
+    vim.api.nvim_set_hl(namespace_id, "FullyChecked", {
+        bg = cfg.colors.fully_checked,
+    })
+    vim.api.nvim_set_hl(namespace_id, "InProgress", {
+        bg = cfg.colors.in_progress,
+    })
+    -- Is this highlight useful?
+    vim.api.nvim_set_hl(namespace_id, "Scheduled", {
+        bg = cfg.colors.scheduled,
+    })
+
     vim.api.nvim_create_autocmd({"BufEnter"}, {
         pattern = {"*.fst", "*.fsti"},
         callback = function()
-            setup_lsp(cfg.fstar_lsp_path)
+            setup_lsp(cfg.fstar_lsp_path, namespace_id)
             setup_fstar_command()
         end,
     })
---
+++ lua/status.lua
@@ -0,0 +1,120 @@
+-- F*-LSP sends us two type of verification status update:
+-- - clear all status
+-- - set some range to some status (fully checked, lax checked, or failed)
+-- This module handle status while working around two shortcomings of status given by F*-LSP.
+-- First, the status ranges are not connected, hence leaving "holes" which are ugly.
+-- Second, when modifying a verified portion of the code,
+-- F* needs to tell us what portions of the code aren't verified anymore.
+-- It is done by sending a "clear" operation,
+-- quickly followed by many "set status" operations for portions that stay verified,
+-- slowly followed by other "set status" operations for new code being verified.
+-- The "clear" operation followed by many "set status" operations cause some flicker,
+-- Because there is no way to distinguish "still-checked" status updates and
+-- "under verification" status updates other than the speed at which they occur,
+-- when clearing status, we buffer them for a short period of time (10ms) to remove any flickering.
+--
+-- The whole module rely on the following invariant from F*-LSP:
+-- - after each clear, the status updates come in increasing order
+-- - a "failed" status update is the last one (before the next clear)
+
+local StatusDisplayer = {}
+
+local hl_group_tbl = {
+    InProgress = "InProgress",
+    LaxOk = "InProgress", --TODO other color
+    Ok = "FullyChecked",
+}
+
+function StatusDisplayer:new(namespace_id, buf_nr)
+    local res = {
+        namespace_id = namespace_id,
+        buf_nr = buf_nr,
+        is_waiting = false,
+        status_list = {}
+    }
+    setmetatable(res, self)
+    self.__index = self
+    return res
+end
+
+function StatusDisplayer:clear()
+    self.is_waiting = true
+    self.status_list = {}
+    -- wait 10ms before clearing and drawing next status updates,
+    -- to prevent flickering
+    local timer = vim.loop.new_timer()
+    timer:start(10, 0, function()
+        timer:stop()
+        timer:close()
+        vim.schedule(function ()
+            -- clear all marks
+            for i = 0, vim.api.nvim_buf_line_count(0) do
+                local extmark_id = i+1
+                vim.api.nvim_buf_del_extmark(0, self.namespace_id, extmark_id)
+            end
+            -- draw the buffered status updates
+            for i = 1, #self.status_list do
+                self:draw_status(i)
+            end
+            -- don't delay future status updates
+            self.is_waiting = false
+        end)
+    end)
+end
+
+function StatusDisplayer:draw_status(status_ind)
+    local start_line = self.status_list[status_ind].start_line
+    local end_line = self.status_list[status_ind].end_line
+    local status_type = self.status_list[status_ind].status_type
+
+    -- draw between the last and current status update
+    if 1 < status_ind then
+        local previous_status_type = self.status_list[status_ind-1].status_type
+        local after_previous_section = start_line
+        after_previous_section = self.status_list[status_ind-1].end_line+1
+
+        local between_sections_status_type = previous_status_type
+
+        -- do not draw between sections if there is nothing between
+        -- (this condition can also be false when F* sends InProgress followed by Ok for the same range)
+        if after_previous_section < start_line then
+            for i = after_previous_section, start_line-1 do
+                local extmark_id = i+1
+                vim.api.nvim_buf_set_extmark(0, self.namespace_id, i, 0, {
+                    id = extmark_id,
+                    line_hl_group = hl_group_tbl[between_sections_status_type]
+                })
+            end
+        end
+    end
+
+    -- draw for the current status update
+    for i = start_line, end_line do
+        -- The extmark id cannot be 0, add one to avoid that case
+        local extmark_id = i+1
+        if status_type == "Failed" then
+            vim.api.nvim_buf_del_extmark(0, self.namespace_id, extmark_id)
+        else
+            vim.api.nvim_buf_set_extmark(0, self.namespace_id, i, 0, {
+                id = extmark_id,
+                line_hl_group = hl_group_tbl[status_type]
+            })
+        end
+    end
+end
+
+function StatusDisplayer:set_status(start_line, end_line, status_type)
+    table.insert(self.status_list, {
+        start_line = start_line,
+        end_line = end_line,
+        status_type = status_type,
+    })
+    -- don't draw the status yet if we are shortly after a clear
+    if not self.is_waiting then
+        self:draw_status(#self.status_list)
+    end
+end
+
+return {
+    StatusDisplayer = StatusDisplayer,
+}
